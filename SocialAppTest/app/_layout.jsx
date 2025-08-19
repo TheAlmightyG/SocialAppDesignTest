cfa7d58f1329d8637b64b0752ae40986175b0e67
TheAlmightyG <Dylan.M.Waters@hotmail.com>
2025-08-19 18:53:23 -0400

    Beginning to add login info

---
+++ SocialAppTest/app/_layout.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
+
+const _layout = () => {
+  return (
+   <Stack
+    screenOptions={{
+        headerShown: false
+    }}
+    />
+  )
+}
+
+export default _layout
+
---
+++ SocialAppTest/app/login.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import ScreenWrapper from '../components/ScreenWrapper'
+import Home from '../assets/icons/Home'
+import { theme } from '../constants/Theme'
+const Login = () => {
+  return (
+    <ScreenWrapper>
+      <Text>Login</Text>
+      <Home strokeWidth={2} color={theme.colors.primary}/>
+    </ScreenWrapper>
+  )
+}
+
+export default Login
+
+const styles = StyleSheet.create({})
